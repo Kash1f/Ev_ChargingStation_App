6a74226bb6bf5606579871226de3226b0757b23c
Kashif <ahmed.kashif1001@gmail.com>
2024-02-16 22:57:10 +0500

    fonts and useCallback

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
 
 export default function App() {
+
+  const [fontsLoaded, fontError] = useFonts({
+    'Outfit': require('./assets/fonts/Outfit-Regular.ttf'),
+    'Outfit-medium': require('./assets/fonts/Outfit-SemiBold.ttf'),
+    'Outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
